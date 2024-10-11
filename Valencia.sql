fff76b5a8988c97728a06b8a1cd9cfb477a19a53
Raúl <raulsarmientoperez@gmail.com>
2024-10-12 00:51:21 +0100

    Create Valencia.sql
    Base datos creada por Raúl

---
+++ Valencia.sql
@@ -0,0 +1,442 @@
+
+("2", "Valencia Mestalla", "1923", "La Paterna");
+
+
+
+insert into entrenador(id_entrenador, nombre, nacionalidad, fecha_nacimiento, id_equipo)
+
+values
+
+("1", "Ruben Baraja", "España", "1975-07-11","1"),
+
+("2", "Chema Sanz", "España", "1969-09-24", "1"),
+
+("3", "Toni Seligrat", "España", "1968-10-20", "1"),
+
+("4", "José Manuel Ochotorena ", "España", "1961-01-16", "1"),
+
+("5", "Juan Monar", "España", "1980-02-05", "1"),
+
+("6", "Miguel Anguel Angulo", "España", "1973-08-13", "2"),
+
+("7", "Raúl Pérez", "España", "1988-07-28", "2"),
+
+("8", "Javi Venta", "Andorra", "1976-01-31", "2"),
+
+("9", "Sergi Benet", "Portugal", "1990-09-23", "2"),
+
+("10", "Carlos Arroyo", "Angola", "1985-05-14", "2");
+
+
+
+insert into jugador(id_jugador, nombre, posicion, nacionalidad, fecha_nacimiento, id_equipo, altura, dorsal)
+
+values 
+
+("1", "Giorgi Mamardashvili", "Portero", "Georgia", "2000-09-29", "1", "197", "25"),
+
+("2", "Jaume Domenech", "Portero", "España", "1990-11-05", "1", "185", "1"),
+
+("3", "Cristian Rivero", "Portero", "España", "1998-03-21", "1", "188", "13"),
+
+("4", "Cristhian Mosquera", "Central", "España", "2004-06-27", "1", "191", "3"),
+
+("5", "Cenk Ozkacar", "Central", "Turquia", "2000-10-06", "1", "190", "15"),
+
+("6", "Yarek Gasiorowski", "Central", "España", "2005-01-12", "1", "192", "34"),
+
+("7", "Mouctar Diakhaby", "Central", "Guinea", "1996-12-19", "1", "192", "4"),
+
+("8", "José Gayá", "Lateral", "España", "1995-05-25", "1", "172", "14"),
+
+("9", "Jesús Váquez", "Lateral", "España", "2003-01-02", "1", "182", "21"),
+
+("10", "Thierry Correia", "Lateral", "Portugal", "1999-03-09", "1", "176", "12"),
+
+("11", "Dimitri Foulquier", "Lateral", "Guadalupe", "1993-03-23", "1", "183", "20"),
+
+("12", "Pepelu", "Centrocampista", "España", "1998-08-11", "1", "186", "18"),
+
+("13", "Hugo Guillamon", "Centrocampista", "España", "2000-01-31", "1", "178", "6"),
+
+("14", "André Almeida", "Centrocampista", "Portugal", "2000-05-30", "1", "176", "10"),
+
+("15", "Selim Amallah", "Centrocampista", "Marruecos", "1996-11-15", "1", "185", "19"),
+
+("16", "Pablo Gozálbez", "Centrocampista", "España", "2001-04-30", "1", "182", "18"),
+
+("17", "Diego Lopez", "Extremo", "España", "2002-05-13", "1", "172", "16"),
+
+("18", "Fran Pérez", "Extremo", "España", "2002-09-09", "1", "176", "23"),
+
+("19", "Sergi Canós", "Extremo", "España", "1997-02-02", "1", "177", "7"),
+
+("20", "Peter Federico", "Extremo", "República Dominicana", "2002-07-25", "1", "180", "11"),
+
+("21", "Hugo González", "Extremo", "España", "2003-02-07", "1", "179", "17"),
+
+("22", "Hugo Duro", "Delantero", "España", "1999-11-10", "1", "183", "9"),
+
+("23", "Roman Yaremchuk", "Delantero", "Ucrania", "1995-11-27","1", "191", "17"),
+
+("24", "Alberto Marí", "Delantero", "España", "2001-07-11","1", "183", "22"),
+
+("25", "Luis García", "Portero", "España", "2003-06-15", "2", "188", "31"),
+
+("26", "Andrei Popov", "Portero", "Rusia", "2002-03-24", "2", "185", "32"),
+
+("27", "Marko Vukovic", "Portero", "Serbia", "2004-09-07", "2", "190", "33"),
+
+("28", "Mateo Fernández", "Central", "Argentina", "2001-11-29", "2", "187", "36"),
+
+("29", "David López", "Central", "España", "2003-02-18", "2", "189", "37"),
+
+("30", "Nicolas Santos", "Central", "Brasil", "2000-08-10", "2", "192", "38"),
+
+("31", "Leonardo Rossi", "Central", "Italia", "2002-05-03", "2", "190", "39"),
+
+("32", "Fernando Ruiz", "Lateral", "España", "2001-04-05", "2", "176", "40"),
+
+("33", "Lukas Novak", "Lateral", "República Checa", "2000-12-12", "2", "178", "41"),
+
+("34", "Ricardo Costa", "Lateral", "Portugal", "2003-09-21", "2", "180", "42"),
+
+("35", "Yusuf Demir", "Lateral", "Austria", "2003-05-02", "2", "173", "43"),
+
+("36", "Gabriel Mendez", "Centrocampista", "Uruguay", "2000-11-14", "2", "181", "44"),
+
+("37", "Mohamed Ali", "Centrocampista", "Egipto", "2002-07-28", "2", "179", "45"),
+
+("38", "Alessandro Rossi", "Centrocampista", "Italia", "2001-01-03", "2", "177", "46"),
+
+("39", "Martin Martinez", "Centrocampista", "Argentina", "2004-03-17", "2", "176", "47"),
+
+("40", "Juan Sanchez", "Extremo", "España", "2003-10-29", "2", "175", "48"),
+
+("41", "Dimitri Petrov", "Extremo", "Rusia", "2002-08-22", "2", "180", "49"),
+
+("42", "Tomas Costa", "Extremo", "Argentina", "2001-06-09", "2", "182", "50"),
+
+("43", "Eduardo Gomez", "Extremo", "Colombia", "2000-12-04", "2", "178", "51"),
+
+("44", "Lucas Fernandez", "Delantero", "España", "2001-09-12", "2", "184", "52"),
+
+("45", "Felipe Rodrigues", "Delantero", "Brasil", "2002-04-07", "2", "186", "53"),
+
+("46", "Santiago Gonzalez", "Delantero", "Argentina", "2003-01-19", "2", "181", "54"),
+
+("47", "Matias Perez", "Delantero", "Uruguay", "2004-07-23", "2", "179", "55"),
+
+("48", "Joaquín Lopez", "Delantero", "España", "2003-11-08", "2", "183", "56");
+
+
+
+
+
+INSERT INTO partido(id_partido, fecha, hora, resultado)
+
+VALUES 
+
+    (1, '2023-08-13', '20:00', '2-1'),
+
+    (2, '2023-08-20', '19:30', '3-0'),
+
+    (3, '2023-08-27', '20:00', '1-1'),
+
+    (4, '2023-09-03', '21:00', '0-2'),
+
+    (5, '2023-09-17', '18:30', '1-3'),
+
+    (6, '2023-09-24', '19:45', '1-1'),
+
+    (7, '2023-09-27', '20:00', '2-0'),
+
+    (8, '2023-10-01', '18:15', '1-1'),
+
+    (9, '2023-10-15', '19:00', '2-1'),
+
+    (10, '2023-10-22', '19:45', '0-1'),
+
+    (11, '2023-10-29', '20:30', '3-2'),
+
+    (12, '2023-11-05', '18:45', '1-0'),
+
+    (13, '2023-11-19', '19:15', '2-2'),
+
+    (14, '2023-11-26', '20:00', '0-1'),
+
+    (15, '2023-12-03', '18:30', '2-0'),
+
+    (16, '2023-12-10', '19:45', '1-3'),
+
+    (17, '2023-12-17', '20:30', '1-0'),
+
+    (18, '2023-12-22', '20:00', '1-1'),
+
+    (19, '2024-01-07', '18:15', '0-2'),
+
+    (20, '2024-01-14', '19:30', '0-1'),
+
+    (21, '2024-01-21', '20:15', '2-1'),
+
+    (22, '2024-01-28', '21:00', '0-2'),
+
+    (23, '2024-02-04', '18:30', '1-2'),
+
+    (24, '2024-02-11', '19:45', '1-3'),
+
+    (25, '2024-02-18', '20:00', '2-0'),
+
+    (26, '2024-02-25', '18:45', '1-0'),
+
+    (27, '2024-03-03', '19:15', '2-2'),
+
+    (28, '2024-03-10', '20:00', '3-1'),
+
+    (29, '2024-03-17', '18:30', '1-1'),
+
+    (30, '2024-03-31', '19:45', '2-0'),
+
+    (31, '2024-04-07', '20:30', '1-1'),
+
+    (32, '2024-04-14', '20:00', '2-1'),
+
+    (33, '2024-04-21', '18:15', '0-2'),
+
+    (34, '2024-04-28', '19:30', '1-0'),
+
+    (35, '2024-05-05', '20:15', '0-2'),
+
+    (36, '2024-05-12', '21:00', '1-2'),
+
+    (37, '2024-05-19', '18:30', '0-1'),
+
+    (38, '2024-05-26', '19:45', '2-0'),
+
+    (39, '2023-08-13', '21:30', '3-2'),
+
+    (40, '2023-08-20', '18:45', '2-0'),
+
+    (41, '2023-08-27', '21:15', '0-0'),
+
+    (42, '2023-09-03', '20:30', '1-1'),
+
+    (43, '2023-09-17', '19:00', '2-1'),
+
+    (44, '2023-09-24', '20:15', '1-0'),
+
+    (45, '2023-09-27', '21:30', '3-1'),
+
+    (46, '2023-10-01', '19:45', '1-2'),
+
+    (47, '2023-10-15', '20:00', '0-3'),
+
+    (48, '2023-10-22', '21:15', '1-0'),
+
+    (49, '2023-10-29', '20:45', '2-2'),
+
+    (50, '2023-11-05', '21:30', '0-1'),
+
+    (51, '2023-11-19', '20:00', '2-3'),
+
+    (52, '2023-11-26', '21:15', '1-1'),
+
+    (53, '2023-12-03', '20:30', '2-2'),
+
+    (54, '2023-12-10', '19:45', '0-3'),
+
+    (55, '2023-12-17', '20:00', '1-0'),
+
+    (56, '2023-12-22', '21:15', '2-1'),
+
+    (57, '2024-01-07', '20:45', '1-1'),
+
+    (58, '2024-01-14', '20:00', '0-2'),
+
+    (59, '2024-01-21', '21:30', '3-1'),
+
+    (60, '2024-01-28', '18:45', '1-0'),
+
+    (61, '2024-02-04', '20:15', '2-0'),
+
+    (62, '2024-02-11', '19:00', '1-1'),
+
+    (63, '2024-02-18', '20:30', '0-2'),
+
+    (64, '2024-02-25', '21:45', '1-3'),
+
+    (65, '2024-03-03', '20:00', '2-1'),
+
+    (66, '2024-03-10', '19:15', '0-0'),
+
+    (67, '2024-03-17', '20:45', '1-2'),
+
+    (68, '2024-03-31', '20:00', '2-0'),
+
+    (69, '2024-04-07', '21:30', '3-1'),
+
+    (70, '2024-04-14', '18:45', '1-0'),
+
+    (71, '2024-04-21', '20:15', '0-2'),
+
+    (72, '2024-04-28', '19:30', '2-1'),
+
+    (73, '2024-05-05', '20:45', '1-1'),
+
+    (74, '2024-05-12', '20:00', '0-3'),
+
+    (75, '2024-05-19', '21:30', '2-2'),
+
+    (76, '2024-05-26', '18:45', '1-0');    
+
+    
+
+INSERT INTO partidoequipo(id_partido, id_equipo)
+
+VALUES 
+
+    (1, 1), (2, 1), (3, 1), (4, 1), (5, 1), (6, 1), (7, 1), (8, 1),
+
+    (9, 1), (10, 1), (11, 1), (12, 1), (13, 1), (14, 1), (15, 1), (16, 1),
+
+    (17, 1), (18, 1), (19, 1), (20, 1), (21, 1), (22, 1), (23, 1), (24, 1),
+
+    (25, 1), (26, 1), (27, 1), (28, 1), (29, 1), (30, 1), (31, 1), (32, 1),
+
+    (33, 1), (34, 1), (35, 1), (36, 1), (37, 1), (38, 1),
+
+    (39, 2), (40, 2), (41, 2), (42, 2), (43, 2), (44, 2), (45, 2), (46, 2),
+
+    (47, 2), (48, 2), (49, 2), (50, 2), (51, 2), (52, 2), (53, 2), (54, 2),
+
+    (55, 2), (56, 2), (57, 2), (58, 2), (59, 2), (60, 2), (61, 2), (62, 2),
+
+    (63, 2), (64, 2), (65, 2), (66, 2), (67, 2), (68, 2), (69, 2), (70, 2),
+
+    (71, 2), (72, 2), (73, 2), (74, 2), (75, 2), (76, 2);
+
+    
+
+
+
+INSERT INTO lesion (tipo, fecha_inicio, fecha_recuperacion, id_jugador)
+
+VALUES 
+
+    ('Esguince de tobillo', '2023-09-01', '2023-10-15', 5),
+
+    ('Fractura de brazo', '2023-10-05', '2024-01-20', 7),
+
+    ('Distensión muscular', '2023-11-10', '2023-12-30', 1),
+
+    ('Rotura de ligamento', '2024-01-02', '2024-05-01', 9),
+
+    ('Contractura lumbar', '2024-02-15', '2024-03-20', 35),
+
+    ('Esguince de rodilla', '2024-03-10', '2024-04-25', 18),
+
+    ('Conmoción cerebral', '2023-08-20', '2023-09-30', 22),
+
+    ('Desgarro muscular', '2023-09-25', '2023-11-10', 14),
+
+    ('Tendinitis rotuliana', '2023-12-05', '2024-02-15', 30),
+
+    ('Contractura cervical', '2024-01-15', '2024-03-10', 3),
+
+    ('Esguince de muñeca', '2023-10-30', '2023-12-20', 8),
+
+    ('Fractura de tibia', '2023-11-20', '2024-03-01', 12),
+
+    ('Distensión de hombro', '2024-01-10', '2024-02-25', 33),
+
+    ('Rotura de menisco', '2024-02-20', '2024-05-10', 24),
+
+    ('Lumbalgia crónica', '2023-09-15', '2023-12-10', 17),
+
+    ('Lesión de tobillo', '2023-10-25', '2024-02-20', 20),
+
+    ('Lesión en la espalda', '2024-01-05', '2024-04-15', 10),
+
+    ('Esguince de codo', '2023-11-05', '2024-01-30', 2),
+
+    ('Fractura de peroné', '2024-02-10', '2024-06-01', 25),
+
+    ('Tendinitis Aquilea', '2023-08-30', '2023-10-25', 19),
+
+    ('Lesión en el talón', '2023-12-20', '2024-03-05', 28),
+
+    ('Contractura en la pierna', '2024-01-20', '2024-04-10', 38),
+
+    ('Rotura de tendón', '2023-10-15', '2024-01-25', 11),
+
+    ('Esguince de dedo', '2024-02-05', '2024-05-15', 6);
+
+
+
+INSERT INTO patrocinador (id_patrocinador, nombre, tipo_patrocinio, duracion_mes, id_equipo)
+
+VALUES 
+
+    (1, 'Adidas', 'Patrocinio de indumentaria', 7, 1),
+
+    (2, 'Puma', 'Patrocinio de indumentaria', 13, 2),
+
+    (3, 'Coca-Cola', 'Patrocinio de bebidas', 29, 1),
+
+    (4, 'Pepsi', 'Patrocinio de bebidas', 14, 2),
+
+    (5, 'Samsung', 'Patrocinio tecnológico', 3, 1),
+
+    (6, 'SEAT', 'Patrocinio automovilístico', 16, 2),
+
+    (7, 'McDonalds', 'Patrocinio alimenticio', 21, 1),
+
+    (8, 'El Corte Inglés', 'Patrocinio de moda y deportes', 7, 1),
+
+    (9, 'Toyota', 'Patrocinio automovilístico', 13, 2),
+
+    (10, 'Zara', 'Patrocinio de moda y deportes', 29, 1),
+
+    (11, 'Telefónica', 'Patrocinio de telecomunicaciones', 14, 2),
+
+    (12, 'BBVA', 'Patrocinio financiero', 3, 1),
+
+    (13, 'Iberdrola', 'Patrocinio energético', 16, 2),
+
+    (14, 'Mercadona', 'Patrocinio alimenticio', 21, 1),
+
+    (15, 'Movistar', 'Patrocinio de telecomunicaciones', 7, 1),
+
+    (16, 'Mapfre', 'Patrocinio financiero', 13, 2),
+
+    (17, 'Renfe', 'Patrocinio de transporte', 29, 1),
+
+    (18, 'Inditex', 'Patrocinio de moda y deportes', 14, 2),
+
+    (19, 'Repsol', 'Patrocinio automovilístico', 3, 1);
+
+    
+
+INSERT INTO aficion (id_aficion, nombre, ubicacion, id_equipo)
+
+VALUES 
+
+    (1, 'Los Murciélagos', 'Grada Nord', 1),
+
+    (2, 'Los Che', 'Grada Sud', 1),
+
+    (3, 'Los Naranjeros', 'Lateral Este', 1),
+
+    (4, 'Los Valencianistas', 'Lateral Oeste', 1),
+
+    (5, 'Los Tarongers', 'Preferencia Nord', 1),
+
+    (6, 'Los Blanquinegres', 'Preferencia Sud', 1),
+
+    (7, 'Los Chés', 'Tribuna Nord', 1),
+
+    (8, 'Los Mestallistas', 'Tribuna Sud', 1),
+
+    (9, 'Los Valencianos', 'Gol Norte', 1),
+
+    (10, 'Los Toronjeros', 'Gol Sur', 1);
